3331f8c9e3cae5bd7fe899f41df9e14b1303b170
Sebastien Viale <sebastien.viale1@michelin.com>
2023-10-27 11:46:01 +0200

    init streams staus in probe

--- src/main/java/com/bdxio/stream/controller/ProbeController.java
+++ src/main/java/com/bdxio/stream/controller/ProbeController.java
@@ -1,34 +1,42 @@
 package com.bdxio.stream.controller;
 
+import org.apache.kafka.streams.KafkaStreams;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.bdxio.stream.BdxIoStream;
 
 @RestController
 public class ProbeController {
 
-    private final HealthEndpoint healthEndpoint;
+    private final BdxIoStream bdxioStream;
 
-    public ProbeController(HealthEndpoint healthEndpoint) {
-        this.healthEndpoint = healthEndpoint;
+    public ProbeController(BdxIoStream bdxioStream) {
+        this.bdxioStream = bdxioStream;
     }
 
     @GetMapping("/readiness")
     public ResponseEntity<String> readinessProbe() {
-        if(Status.UP.equals(healthEndpoint.health().getStatus())){
-            return ResponseEntity.status(HttpStatus.OK).body("Readiness OK");
-        }
+            if (bdxioStream.getStreams() != null && bdxioStream.getStreams().state()
+                    == KafkaStreams.State.RUNNING) {
+                return ResponseEntity.status(HttpStatus.OK).body("Readiness OK");
+            }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
     }
 
     @GetMapping("/liveness")
     public ResponseEntity<String> livenessProbe() {
-        if(Status.UP.equals(healthEndpoint.health().getStatus())){
+        if (bdxioStream.getStreams() != null &&
+                bdxioStream.getStreams().state()
+                == KafkaStreams.State.RUNNING ||
+                bdxioStream.getStreams().state()
+                        == KafkaStreams.State.REBALANCING) {
             return ResponseEntity.status(HttpStatus.OK).body("Liveness OK");
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
     }
+
 }
